15d07d1b8a53c3c84c7149d43596f85bd91efbfe
komat0 <111368527+komat0@users.noreply.github.com>
2023-03-31 14:01:00 +0700

    Homework 4.3 DONE

---
+++ src/GeometryCalculator.java
@@ -0,0 +1,37 @@
+public class GeometryCalculator {
+    // метод должен использовать абсолютное значение radius
+    public static double getCircleSquare(double radius) {
+        double S = Math.PI * Math.pow(Math.abs(radius), 2);
+        S = Math.round(S * 100.0) / 100.0;
+        return S;
+    }
+
+    // метод должен использовать абсолютное значение radius
+    public static double getSphereVolume(double radius) {
+        double S = 4 * Math.PI * Math.pow(Math.abs(radius), 2);
+        S = Math.round(S * 100.0) / 100.0;
+        return S;
+    }
+
+    public static boolean isTriangleRightAngled(double a, double b, double c) {
+
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+            return true;
+        }
+        return false;
+    }
+
+    // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
+    // методом isTriangleRightAngled, если невозможен вернуть -1.0
+    public static double getTriangleSquare(double a, double b, double c) {
+
+        if (isTriangleRightAngled(a, b, c)) {
+            double halfPerimeter = (a + b + c) / 2;
+            double perimeter = Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b)
+                    * (halfPerimeter - c));
+            return Math.round(perimeter * 100.0) / 100.0;
+        }
+        System.out.print("Треугольник невозможно построить, код ");
+        return -1;
+    }
+}
